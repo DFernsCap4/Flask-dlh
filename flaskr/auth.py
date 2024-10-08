ff8c92af959de3292a8dd3a122c47d9780943012
DFernsCap4 <david.ferns@cap4lab.com>
2024-10-08 15:51:05 +0200

    Added more views to auth

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,6 +10,17 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id)
+        ).fetchone()
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == "POST":
@@ -36,3 +47,30 @@ def register():
                 return redirect(url_for('auth.login'))
         flash(error)
     return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password= request.form['password']
+        db = get_db()
+        user = db.execute(
+            'SELECT * FROM users WHERE username = ?', (username)
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username or password."
+        elif not check_password_hash(user['password'], password):
+            error = "Incorrect username or password."
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        flash(error)
+    return render_template('auth/login.html')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
